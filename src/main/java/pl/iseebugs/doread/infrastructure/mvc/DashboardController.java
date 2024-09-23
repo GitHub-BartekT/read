dfd298a634281f5ac1270f3b796d098f7aa4f2ab
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-09-23 11:14:31 +0200

    Add redirected after positive login

--- src/main/java/pl/iseebugs/doread/infrastructure/mvc/DashboardController.java
+++ src/main/java/pl/iseebugs/doread/infrastructure/mvc/DashboardController.java
@@ -1,18 +1,19 @@
 package pl.iseebugs.doread.infrastructure.mvc;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class DashboardController {
 
     @GetMapping("/dashboard")
-    public ResponseEntity<String> getDashboard() {
+    public String getDashboard() {
         // Zwracana strona po autoryzacji
-        return ResponseEntity.ok("Witaj na dashboardzie, dostęp chroniony!");
+        return "dashboard";
     }
 }
 
