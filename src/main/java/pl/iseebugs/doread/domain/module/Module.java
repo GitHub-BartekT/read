f24a0f7d1563ccab81bcba9bf86e88a1dda479df
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-09-23 11:46:32 +0200

    Add entity

---
+++ src/main/java/pl/iseebugs/doread/domain/module/Module.java
@@ -0,0 +1,35 @@
+package pl.iseebugs.doread.domain.module;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "modules")
+@NoArgsConstructor
+@AllArgsConstructor
+class Module {
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "module_name")
+    private String moduleName;
+    @Column(name = "sessions_Per_Day")
+    private int sessionsPerDay;
+    @Column(name = "presentations_Per_Session")
+    private int presentationsPerSession;
+    @Column(name = "new_Sentences_Per_Day")
+    private int newSentencesPerDay;
+    @Column(name = "actual_Day")
+    private int actualDay;
+    @Column(name = "next_Session")
+    private int nextSession;
+    @Column (name = "is_private")
+    private boolean isPrivate;
+}
