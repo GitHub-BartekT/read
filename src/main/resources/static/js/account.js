a6f156967a995602553008e52e60fd660417e902
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-10-13 00:21:31 +0200

    Add delete account

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,3 +1,5 @@
+const API_URL_DELETE = API_URL + '/auth/delete';
+
 function logout() {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
@@ -6,3 +8,27 @@ function logout() {
 }
 
 document.getElementById('logout-button').addEventListener('click', logout);
+
+document.getElementById('delete-account-btn').addEventListener('click', () => {
+    const confirmDelete = confirm('Czy na pewno chcesz usunąć kont? Po potwierdzeniu zostaniesz wylogowany, a na twój email zostanie wysłana wiadomość z prośbą o potwierdzenie chęci usunięcia konta.');
+    if (confirmDelete) {
+        deleteAccount();
+        logout();
+    }
+});
+
+function deleteAccount() {
+    const token = localStorage.getItem('accessToken');
+
+    fetch(API_URL_DELETE, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .catch(error => {
+            console.error('Error deleting module:', error.message);
+        });
+}
