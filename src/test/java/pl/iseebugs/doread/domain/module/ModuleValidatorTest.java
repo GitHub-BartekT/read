8ee799baa600b89b8290e325c219499e3dff076f
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-10-13 20:35:06 +0200

    Add longValidator tests

--- src/test/java/pl/iseebugs/doread/domain/module/ModuleValidatorTest.java
+++ src/test/java/pl/iseebugs/doread/domain/module/ModuleValidatorTest.java
@@ -1,13 +1,69 @@
 package pl.iseebugs.doread.domain.module;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ModuleValidatorTest {
 
+    private ModuleValidator moduleValidator;
+
+    @BeforeEach
+    void setUp() {
+        moduleValidator = new ModuleValidator();
+    }
+
     @Test
-    void longValidator() {
+    @DisplayName("longValidator should throws IllegalArgumentException when argument is null")
+    void longValidator_throws_IllegalArgumentException_when_argument_is_null() {
+        //Given
+        Long argument = null;
+        String message = "bar";
+
+        //When
+        Throwable e = catchThrowable(() -> moduleValidator.longValidator(argument, message));
+
+        // Then
+        assertAll(
+                () -> assertThat(e).isInstanceOf(IllegalArgumentException.class),
+                () -> assertThat(e.getMessage()).isEqualTo(message)
+        );
+    }
+
+    @Test
+    @DisplayName("longValidator should throws IllegalArgumentException when argument is less then 1")
+    void longValidator_throws_IllegalArgumentException_when_argument_is_less_then_1() {
+        //Given
+        Long argument = 0L;
+        String message = "foo";
+
+        //When
+        Throwable e = catchThrowable(() -> moduleValidator.longValidator(argument, message));
+
+        // Then
+        assertAll(
+                () -> assertThat(e).isInstanceOf(IllegalArgumentException.class),
+                () -> assertThat(e.getMessage()).isEqualTo(message)
+        );
+    }
+
+    @Test
+    @DisplayName("longValidator should does nothing after validation")
+    void longValidator_does_nothing() {
+        //Given
+        Long argument = 2L;
+        String message = "foo";
+
+        //When
+        moduleValidator.longValidator(argument, message);
+
+        // Then
+        assertDoesNotThrow(() -> moduleValidator.longValidator(argument, message));
     }
 
     @Test
