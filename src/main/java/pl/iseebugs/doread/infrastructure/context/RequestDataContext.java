bcef7cbd08b4173efeb7504a9d9eb02f324e4ff8
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-10-28 23:20:54 +0100

    Add requestDataContext object

---
+++ src/main/java/pl/iseebugs/doread/infrastructure/context/RequestDataContext.java
@@ -0,0 +1,20 @@
+package pl.iseebugs.doread.infrastructure.context;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.List;
+
+@Component
+@RequestScope
+@Data
+@Getter
+@Setter
+public class RequestDataContext {
+    private Long userId;
+    private Long sessionId;
+    private List<Long> moduleId;
+}
