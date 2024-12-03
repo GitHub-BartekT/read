c16ed162156faf29b54b22b132edcf5a8362c6e6
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-12-03 15:10:00 +0100

    Redraw account.html

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -7,12 +7,82 @@ function logout() {
 
 document.getElementById('logout-button').addEventListener('click', logout);
 
+document.getElementById('acceptNewPassword').addEventListener('click', changePassword);
+
+document.getElementById('newPassword2').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        changePassword();
+    }
+});
+
+function changePassword() {
+    if (!checkPasswords()) {
+        return;
+    }
+
+    setElementDisabled('newPassword1', true);
+    setElementDisabled('newPassword2', true);
+    setElementDisabled('acceptNewPassword', true);
+    const button = document.getElementById("acceptNewPassword");
+    button.classList.remove("green-button");
+    button.classList.add("grey-button");
+    showError("");
+}
+
+function checkPasswords() {
+    const password1 = document.getElementById('newPassword1').value;
+    const password2 = document.getElementById('newPassword2').value;
+
+    if (!checkPasswordsLength(password1.length)) {
+        return false;
+    }
+
+    if (password1 !== password2) {
+        showError('Hasło muszą być jednakowe.');
+        return false;
+    }
+
+    return true;
+}
+
+function checkPasswordsLength(length) {
+    if (length < 3) {
+        showError('Hasło musi mieć co najmniej 3 znaki.');
+        return false;
+    }
+    return true;
+}
+
+function showError(message) {
+    const errorMessageElement = document.getElementById('errorMessage');
+    errorMessageElement.textContent = message;
+}
+
 document.getElementById('delete-account-btn').addEventListener('click', () => {
     const confirmDelete = confirm('Czy na pewno chcesz usunąć kont? Po potwierdzeniu zostaniesz wylogowany, a na twój email zostanie wysłana wiadomość z prośbą o potwierdzenie chęci usunięcia konta.');
     if (confirmDelete) {
         deleteAccount();
     }
 });
+
+
+document.getElementById('change-password').addEventListener('click', setNewPassword);
+
+function setNewPassword() {
+    setElementDisabled('newPassword1', false);
+    setElementDisabled('newPassword2', false);
+    setElementDisabled('acceptNewPassword', false);
+    const button = document.getElementById("acceptNewPassword");
+    button.classList.remove("grey-button");
+    button.classList.add("green-button");
+}
+
+
+function setElementDisabled(id, param) {
+    const input = document.getElementById(id);
+    input.disabled = param;
+}
+
 
 function deleteAccount() {
     const token = localStorage.getItem('accessToken');
