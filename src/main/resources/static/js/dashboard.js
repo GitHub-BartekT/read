5a0691f97e757ccf4b05cf64ec6c78e8e17e534e
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-12-04 10:48:18 +0100

    Document version

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -9,10 +9,9 @@ function getAllSentences(moduleId) {
     })
         .then(data => {
             let tempParent = document.getElementById("new-session-buttons");
-            tempParent.innerHTML = ''; // Clear any existing buttons
+            tempParent.innerHTML = '';
 
             if (data.data.length === 0) {
-                // If the list is empty, display "Create New Module" button
                 const createNewModuleBtn = document.createElement("button");
                 createNewModuleBtn.className = "button yellow-button";
                 createNewModuleBtn.textContent = "Stwórz nowy moduł";
@@ -21,17 +20,16 @@ function getAllSentences(moduleId) {
                 });
                 tempParent.appendChild(createNewModuleBtn);
             } else {
-                // Loop through the sessions and create buttons
+
                 data.data.forEach((userSession) => {
                     const newSessionBtn = document.createElement("button");
                     newSessionBtn.className = "button";
                     newSessionBtn.textContent = userSession.name;
                     newSessionBtn.id = 'session-' + userSession.id;
 
-                    // You can also add event listeners to each button if needed
                     newSessionBtn.addEventListener('click', () => {
                         console.log(`Session ID: ${userSession.id} was clicked.`);
-                        // Add further action like redirecting or loading session details
+
                     });
                     newSessionBtn.addEventListener('click', () => {
                         redirectToSessionPage(userSession.id);
@@ -47,6 +45,5 @@ function getAllSentences(moduleId) {
 }
 
 function redirectToSessionPage(sessionId) {
-    // Redirect to the session.html page with the sessionId in the URL
     window.location.href = `session.html?sessionId=${sessionId}`;
 }
